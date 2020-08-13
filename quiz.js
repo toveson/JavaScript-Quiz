dc2cfedac746118e34c9173d409be40db3504581
Trever O <toveson9522@gmail.com>
2020-08-12 19:32:01 -0600

    added psuedo code for .js

---
+++ quiz.js
@@ -0,0 +1,26 @@
+// question number
+    // display question you are on and how many questions there are ex 13/20
+
+// take the quiz
+    // Make it a button to start the quiz
+
+// view highscores
+    // make it a button to view high scores
+
+// timer
+    // count down starts when take the quiz is clicked
+    // minus time for wrong answers
+
+// question goes here neeeds to populate with questions 
+    // needs to populate a question when take the quiz is clicked
+    // populate the next question when you select an answer
+    // needs to change spinner color green if answer is correct
+    // needs to change spinner color red if answer is wrong
+    
+    // answer cards 
+        // need to populate with the options for the given question
+
+
+// questions
+    // need to add my questions to an array?
+    // need to know what answer is correct
