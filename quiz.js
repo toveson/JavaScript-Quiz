74b27a6668fc7d6a643072320bd1e4dce92e0913
Trever O <toveson9522@gmail.com>
2020-08-12 22:44:23 -0600

    created a timer

--- quiz.js
+++ quiz.js
@@ -1,30 +1,38 @@
-// question number
-    // display question you are on and how many questions there are ex 13/20
+// global vars
+var startingMinutes = parseInt(20);
+var timeCount = startingMinutes * 60;
 
-// take the quiz
-    // Make it a button to start the quiz
-
-// view highscores
-    // make it a button to view high scores
 
 // timer
-    // count down starts when take the quiz is clicked
-    // minus time for wrong answers
 
-    // how much time to start with 
+// updates timer every second
+setInterval(updateTime, 1000);
 
-    // when time reaches 00:00 make the quiz genarate a score
+function updateTime() {
+    var minutesCount = Math.floor(timeCount / 60);
+    var secondsCount = timeCount % 60;
 
-// question goes here neeeds to populate with questions 
-    // needs to populate a question when take the quiz is clicked
-    // populate the next question when you select an answer
-    // needs to change spinner color green if answer is correct
-    // needs to change spinner color red if answer is wrong
-    
-    // answer cards 
-        // need to populate with the options for the given question
+    secondsCount = secondsCount < 10 ? '0' + secondsCount : secondsCount;
+    document.getElementById('countdown').innerHTML = minutesCount + ':' +secondsCount
+    timeCount--;
+}
+console.log(timeCount);
 
 
-// questions
-    // need to add my questions to an array?
-    // need to know what answer is correct
+// I need an array of High scores
+// I need an array to hold questions
+// I need an array to hold answers for each question
+// I need a function to start the quiz
+// I need a function to control the question number
+// I need a function to display the quiz question
+// I need a function to display the quiz possible answers
+// I need a function to check the selected answer
+   //in this function if the answer is correct, move on to the next question and change color of spinner to green
+   //if the answer is incorrect I need to remove time as a penalty and change the spinner color to red
+   // after moving on to the next question change the question number
+// I need a temporary array to hold the amount of questions ther user got correct
+// when the quiz is complete I will ask the user for their initials
+// after the user submits their initials I will store their score in the High score array (w/initials)
+// if the user beats one of the high scores I need to display their score above the ones they beat
+// I will do this with a function to display the list in order of score from high to low
+
