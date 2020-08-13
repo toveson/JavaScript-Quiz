d109efbff5ebf9d1296aaa1f184303ecd5a14950
Trever O <toveson9522@gmail.com>
2020-08-13 00:47:29 -0600

    added global arrays

--- quiz.js
+++ quiz.js
@@ -1,8 +1,14 @@
 // global vars
 var startingMinutes = parseInt(15);
 var timeCount = startingMinutes * 60;
+
+// global arrays
 // array to store high scores
 var highScores = [];
+// array of questions
+var questions = ['Q1', 'Q2', 'Q3', 'Q4', 'Q5', 'Q6', 'Q7', 'Q8', 'Q9', 'Q10', 'Q11', 'Q12', 'Q13', 'Q14', 'Q15'];
+// array to keep track of how many questions the user answered correct
+var answeredCorrect =[];
 
 // timer
 // updates timer every second
@@ -20,23 +26,24 @@ function updateTime() {
 }
 
 
-// I need an array of High scores
+            //Done// I need an array of High scores
+            // Done// I need an array to hold questions
 
-// I need an array to hold questions
 // I need an array to hold answers for each question
+
 // I need a function to start the quiz
 // I need a function to control the question number
 // I need a function to display the quiz question
 // I need a function to display the quiz possible answers
 // I need a function to check the selected answer
-//in this function if the answer is correct, move on to the next question and change color of spinner to green
-//if the answer is incorrect I need to remove time as a penalty and change the spinner color to red
+    //in this function if the answer is correct, move on to the next question and change color of spinner to green
+    //if the answer is incorrect I need to remove time as a penalty and change the spinner color to red
 // after moving on to the next question change the question number
-// I need a temporary array to hold the amount of questions ther user got correct
+            // Done // I need a temporary array to hold the amount of questions ther user got correct
 // when the quiz is complete I will ask the user for their initials
 // after the user submits their initials I will store their score in the High score array (w/initials)
 // if the user beats one of the high scores I need to display their score above the ones they beat
-// I will do this with a function to display the list in order of score from high to low
+    // I will do this with a function to display the list in order of score from high to low
 
 
 
